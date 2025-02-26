0078250b0dabcd89bbec3658e4faa21788c9a2ac
Luong Duc <vinhduc91@outlook.com>
2025-02-27 01:43:52 +0700

    add required configs for gg sign in

---
+++ front-end/src/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Dashboard from "./Dashboard";
+import { AppQueryClientProvider } from "../providers/QueryClientProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <React.StrictMode>
+    <AppQueryClientProvider>
+      <Dashboard />
+    </AppQueryClientProvider>
+  </React.StrictMode>
+);
