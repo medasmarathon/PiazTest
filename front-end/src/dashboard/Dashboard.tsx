2e0b99dc76e2367a766c97131b9e6bd84e517f93
Luong Duc <vinhduc91@outlook.com>
2025-02-28 01:42:21 +0700

    feat: editable dashboard

--- front-end/src/dashboard/Dashboard.tsx
+++ front-end/src/dashboard/Dashboard.tsx
@@ -20,19 +20,26 @@ import {
 } from "@mui/material";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Cancel";
+import TextField from "@mui/material/TextField";
 import useLinks from "../hooks/useLinks";
-import { TLinkGroup } from "../types";
+import { TLinkGroup, TLinkRequest } from "../types";
 import useAuth from "@/hooks/useAuth";
 
 const Dashboard: React.FC = () => {
   const [selectedGroup, setSelectedGroup] = useState<TLinkGroup | "All">("All");
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const [urlToDelete, setUrlToDelete] = useState<string | null>(null);
+  const [editRow, setEditRow] = useState<
+    (Partial<TLinkRequest> & { created_at: number }) | null
+  >(null);
   const { isLogin, inProgress, userEmail, googleSignIn } = useAuth();
-  const { linksQuery, deleteLink } = useLinks(userEmail);
+  const { linksQuery, deleteLink, saveLink } = useLinks(userEmail);
   const { data: links, isLoading: loading, error } = linksQuery;
 
-  console.log("dashboard render", isLogin)
+  console.log("dashboard render", isLogin);
   if (!isLogin) {
     return (
       <Container maxWidth="lg" sx={{ py: 4, height: "80vh" }}>
@@ -84,25 +91,86 @@ const Dashboard: React.FC = () => {
     {
       field: "actions",
       headerName: "Actions",
-      width: 80,
+      width: 150,
       sortable: false,
       filterable: false,
       renderCell: (params) => (
-        <Tooltip title="Delete">
-          <IconButton
-            onClick={() => handleDeleteClick(params.row.url)}
-            color="error"
-            size="small"
-          >
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
+        <>
+          {editRow?.id === params.row.id ? (
+            <>
+              <Tooltip title="Save">
+                <IconButton
+                  onClick={async () => {
+                    if (editRow) {
+                      await saveLink.mutateAsync({
+                        ...editRow,
+                        created_at: new Date(editRow.created_at).getTime(),
+                      });
+                      setEditRow(null);
+                    }
+                  }}
+                  color="success"
+                  size="small"
+                >
+                  <SaveIcon />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title="Cancel">
+                <IconButton
+                  onClick={() => setEditRow(null)}
+                  color="warning"
+                  size="small"
+                >
+                  <CancelIcon />
+                </IconButton>
+              </Tooltip>
+            </>
+          ) : (
+            <Tooltip title="Edit">
+              <IconButton
+                onClick={() => setEditRow(params.row)}
+                color="primary"
+                size="small"
+              >
+                <EditIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          <Tooltip title="Delete">
+            <IconButton
+              onClick={() => handleDeleteClick(params.row.url)}
+              color="error"
+              size="small"
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        </>
       ),
     },
     {
       field: "title",
       headerName: "Title",
       width: 200,
+      renderCell: (params) => {
+        if (editRow?.id === params.row.id) {
+          return (
+            <TextField
+              value={editRow?.title}
+              onChange={(e) =>
+                setEditRow({
+                  ...editRow,
+                  title: e.target.value,
+                  created_at: params.row.created_at,
+                })
+              }
+              fullWidth
+              size="small"
+            />
+          );
+        }
+        return params.value;
+      },
     },
     {
       field: "url",
@@ -122,18 +190,85 @@ const Dashboard: React.FC = () => {
       field: "group",
       headerName: "Group",
       width: 100,
+      renderCell: (params) => {
+        if (editRow?.id === params.row.id) {
+          return (
+            <Select
+              value={editRow?.group}
+              onChange={(e) =>
+                setEditRow({
+                  ...editRow,
+                  group: e.target.value as TLinkGroup,
+                  created_at: params.row.created_at,
+                })
+              }
+              size="small"
+              fullWidth
+            >
+              <MenuItem value="SaaS">SaaS</MenuItem>
+              <MenuItem value="AI">AI</MenuItem>
+              <MenuItem value="Crypto">Crypto</MenuItem>
+              <MenuItem value="E-commerce">E-commerce</MenuItem>
+            </Select>
+          );
+        }
+        return params.value;
+      },
     },
     {
       field: "description",
       headerName: "Description",
       width: 150,
+      renderCell: (params) => {
+        if (editRow?.id === params.row.id) {
+          return (
+            <TextField
+              value={editRow?.description}
+              onChange={(e) => {
+                debugger;
+                setEditRow({
+                  ...editRow,
+                  description: String(e.target.value),
+                  created_at: params.row.created_at,
+                })
+              }}
+              fullWidth
+              size="small"
+            />
+          );
+        }
+        return params.value;
+      },
     },
     {
       field: "rating",
       headerName: "Rating",
       width: 100,
-      renderCell: (params) =>
-        params.value ? "★".repeat(params.value) : "No rating",
+      renderCell: (params) => {
+        if (editRow?.id === params.row.id) {
+          return (
+            <Select
+              value={editRow?.rating || 0}
+              onChange={(e) =>
+                setEditRow({
+                  ...editRow,
+                  rating: Number(e.target.value),
+                  created_at: params.row.created_at,
+                })
+              }
+              size="small"
+              fullWidth
+            >
+              {[0, 1, 2, 3, 4, 5].map((value) => (
+                <MenuItem key={value} value={value}>
+                  {value ? "★".repeat(value) : "No rating"}
+                </MenuItem>
+              ))}
+            </Select>
+          );
+        }
+        return params.value ? "★".repeat(params.value) : "No rating";
+      },
     },
     {
       field: "created_at",
