5ef9dcf8a72a99c0868b3bd219a0bbfc71b5f4c8
Luong Duc <vinhduc91@outlook.com>
2025-02-28 22:29:03 +0700

    add docker compose for easier local dev

--- back-end/src/data-source.ts
+++ back-end/src/data-source.ts
@@ -3,25 +3,14 @@ import { DataSource } from "typeorm";
 import { LinkModel } from "./model/LinkModel";
 import dotenv from 'dotenv';
 
-let isEnvUnset = process.env.NODE_ENV === undefined;
-
 dotenv.config({ path: `.env` });
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-export const AppDataSource = isEnvUnset ?
-    new DataSource({
-        type: "sqlite",
-        database: `database.${process.env.NODE_ENV}.sqlite`,
-        logging: false,
-        entities: [LinkModel],
-        migrations: ["src/migration/*.[t|j]s"],
-        subscribers: [],
-    }) :
-    new DataSource({
-        type: "postgres",
-        url: process.env.DATABASE_URL,
-        logging: false,
-        entities: [LinkModel],
-        migrations: ["src/migration/*.[t|j]s"],
-        subscribers: [],
-    });
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: false,
+    entities: [LinkModel],
+    migrations: ["src/migration/*.[t|j]s"],
+    subscribers: [],
+});
