e5e97c1490fcb3cceb5ca313f34b2294b954ee90
Luong Duc <vinhduc91@outlook.com>
2025-03-01 22:24:50 +0700

    try fixing missing entity metadata

--- back-end/src/data-source.ts
+++ back-end/src/data-source.ts
@@ -6,12 +6,12 @@ import dotenv from 'dotenv';
 dotenv.config({ path: `.env` });
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
-console.log("path models", path.join(__dirname, "./model/*.[t|j]s"));
+console.log("path models", path.join(__dirname, "model/*{.ts,.js}"));
 export const AppDataSource = new DataSource({
     type: "postgres",
     url: process.env.DATABASE_URL,
     logging: false,
-    entities: [path.join(__dirname, "./model/*.[t|j]s")],
-    migrations: [path.join(__dirname, "./migration/*.[t|j]s")],
+    entities: [path.join(__dirname, "model/*{.ts,.js}")],
+    migrations: [path.join(__dirname, "migration/*{.ts,.js}")],
     subscribers: [],
 });
